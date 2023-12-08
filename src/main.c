01289c9982f3ea49ed3f2d0f65efcfd859f9d17e
ampheee <babbling@student.21-school.ru>
2023-12-08 10:02:46 +0300

    GUI: minor changes for styles. add css file and in-page menu.

--- src/main.c
+++ src/main.c
@@ -15,15 +15,21 @@ G_MODULE_EXPORT void onBtnClicked(GtkButton * btn, gpointer data) {
 
 int main(int argc, char * argv[]) {
     gtk_init(&argc, &argv);
-    GtkBuilder * ui_builder;
+    GtkBuilder *ui_builder;
+    GtkCssProvider *css_provider;
     GError * err = NULL;
     char cwd[516];
     getcwd(cwd, sizeof(cwd));
     ui_builder = gtk_builder_new();
     if(!gtk_builder_add_from_file(ui_builder, "gui/main.glade", &err)) {
-        g_critical ("Не вышло загрузить файл с UI : %s", err->message);
-        g_error_free (err);
+        g_critical("Не вышло загрузить файл с UI : %s", err->message);
+        g_error_free(err);
     }
+    css_provider  = gtk_css_provider_new();
+    if (!gtk_css_provider_load_from_path(css_provider, "gui/styles.css", &err)) {
+        g_critical("Не вышло загрузить файл проводника стилей CSS: %s", err->message);
+        g_error_free(err);
+    } 
     GtkWidget * window = GTK_WIDGET(gtk_builder_get_object(ui_builder, "sapr_main_window"));
     gtk_window_maximize((GtkWindow *) window);
     gtk_builder_connect_signals(ui_builder, NULL);
